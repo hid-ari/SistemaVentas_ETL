7c09db20c8360855409010a653b6950e03335833
Hideki Ariyama <ariyamahideki1@gmail.com>
2025-12-08 23:44:32 -0400

    Recarga de datos

--- SistemaVentasETL-main/SistemaVentasETL-main/SistemaVentas.Etl/Models/DimSalesperson.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaVentas.Etl.Models
-{
-    public class DimSalesperson
-    {
-        public int SalespersonKey { get; set; }
-        public string SalespersonCode { get; set; } = string.Empty;
-        public string SalespersonName { get; set; } = string.Empty;
-    }
-}
